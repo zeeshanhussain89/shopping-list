e7461e4d6024137784d1963a1080a1cef9936674
zeeshanhussain89 <er.zeeshanhussain@gmail.com>
2025-11-14 16:40:35 +0530

    added filter functionality

--- script.js
+++ script.js
@@ -61,6 +61,21 @@ function clearItems() {
   checkUI();
 }
 
+function filterItems(e) {
+  const text = e.target.value.toLowerCase();
+  const items = itemList.querySelectorAll('li');
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+    if (itemName.indexOf(text) !== -1) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+  //   console.log(text);
+}
+
 function checkUI() {
   const items = itemList.querySelectorAll('li');
 
@@ -77,5 +92,6 @@ function checkUI() {
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
 
 checkUI();
